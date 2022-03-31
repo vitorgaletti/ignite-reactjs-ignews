530546f9bda2dfc15dccd036629ae12e6ae0348b
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-31 17:35:40 -0300

    :wrench: nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,6 +13,7 @@ export default NextAuth({
       authorization: { params: { scope: 'read:user' } }
     })
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 
   callbacks: {
     async session({ session }) {
